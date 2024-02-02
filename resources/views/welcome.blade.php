9e45bdc17b3bf19fcca47b50bc41a40e73b6bb8d
Amariah20 <rigodonamariah16@gmail.com>
2024-02-02 17:18:06 +0000

    a

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Appreciation</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('/teddy.css') }}">
+</head>
+<body>
+
+<div class="screen">
+
+    <div class="teddy">
+        <div class="head">
+            <div class="ear1">
+                <div class="ear-m1"></div>
+
+            </div>
+            <div class="ear2">
+            <div class="ear-m2"></div>
+                
+            </div>
+            <div class="eye1">
+            <div class="w"></div>
+
+            </div>
+            <div class="eye2">
+            <div class="w"></div>
+                
+                </div>
+                <div class="mouth_nose">
+                <div class="nose">
+                </div>
+                <div class="mouth">
+                </div>
+                
+                </div>
+    
+
+        </div>
+
+        <div class="body">
+            <div class="stomach">
+            </div>
+            
+            <div class="hand1">
+                <div class="h"></div>
+
+            </div>
+            <div class="hand2">
+            <div class="h"></div>
+            </div>
+
+            <div class="leg1">
+            <div class="l"></div>
+                
+            </div>
+            <div class="leg2">
+            <div class="l"></div>
+                
+            </div>
+            <div class="thought-bubble">
+            <h3 style="font-size: 15px;">I appreciate you, Darnel. Thanks for everything</h3>
+        </div>
+         </div>
+   
+    </div>
+
+    
+
+    
+</div>
+    
+        
+       
+
+
+    
+</body>
+</html>
